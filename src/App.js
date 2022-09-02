fef0b4747106042de41ef41d6db7ac6c8be78a1f
Kostya Petukhov <kosyak971@gmailcom>
2022-09-02 17:33:17 +0300

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+import UsersList from './components/UsersList';
+import UserProfile from './components/UserProfile';
+import UserPosts from './components/UserPosts';
+
 function App() {
-	return <div></div>;
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path='/' element={<Navigate to='/users' replace />} />
+				<Route path='/users' element={<UsersList />} />
+				<Route path='/users/:id' element={<UserProfile />} />
+				<Route path='/users/:id/posts' element={<UserPosts />} />
+				<Route path='*' element={<Navigate to='/users' replace />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
